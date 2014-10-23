b55baa4ff4cde51a89b2966592646c782a92c783
Ryan Xue <auspbro@gmail.com>
2014-10-23 12:10:30 +0800

    first release
    copy input to output, 1st version

---
+++ CTest/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+
+
+/* copy input to output, 1st version*/
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int c;
+	c = getchar();
+	while (c != EOF)
+	{
+		putchar(c);
+		//c = getchar();
+	}
+	return 0;
+}
+
